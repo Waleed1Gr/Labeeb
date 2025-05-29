cc5b5bc9bf4d431c5b7af23cfa4d1ea2619716bb
Naif-Ajeeb <sjejs811@gmail.com>
2025-05-29 12:57:35 +0300

    concealed the secret api key

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+from pathlib import Path
+from SECRET_KEY import API_KEY
+
+# ============== CONFIGURATION ==============
+client = OpenAI(api_key=API_KEY)
+
+speech_file_path = Path("response.mp3")
+speech_file_pathp = Path("phone_warning.mp3")
+DATA_TASKS_FILE = Path("tasks.json")
+wake_word = "لبيب"
